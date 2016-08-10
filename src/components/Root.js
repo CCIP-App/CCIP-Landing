0a262713d7d1e5e90f5182b6d80be5a091eafa52
腹黒い茶 <haraguroicha@tipsy.coffee>
2016-08-10 09:37:57 +0800

    change page title and favicon

--- src/components/Root.js
+++ src/components/Root.js
@@ -7,7 +7,7 @@ class Root extends React.Component {
       <html>
         <head>
           <meta charSet="utf-8" />
-          <title>CCIP App</title>
+          <title>COSCUP Pass</title>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
           <meta name="description" content="" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
